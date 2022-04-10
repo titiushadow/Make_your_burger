a55bd4ff7d2d4e60f3c212e33dde5afd3d26ae91
mesaque <mesaque1212@gmail.com>
2022-04-10 15:57:51 -0300

    Fazendo a importação do componente Banner para a Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
+<Banner />
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu burger:</h1>
+    <p>Formulario</p>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
 
+export default {
+  nome: "Home",
+  components: {
+    Banner   
+  }
+}
 </script>
