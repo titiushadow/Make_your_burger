f79f00f9b6e3c197903c2c43a50bc54523dc1a8e
mesaque <mesaque1212@gmail.com>
2022-04-10 16:52:13 -0300

    Resgatando informações do banco de dados

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -13,7 +13,7 @@
                 <label for="pao">Escolha o pão</label>
                 <select name="pao" id="pao" v-model="pao">
                     <option value="">Selecione o seu pão</option>
-                    <option value="integral">Integral</option>
+                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{pao.tipo}}</option>
                 </select>
            </div>
 
@@ -22,23 +22,15 @@
                 <label for="carne">Escolha a carne do seu Burger</label>
                 <select name="carne" id="carne" v-model="carne">
                     <option value="">Selecione um tipo de carne</option>
-                    <option value="Maminha">Maminha</option>
+                     <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{carne.tipo}}</option>
                 </select>
            </div>
 
             <div id="opcionais-container" class="input-container"> 
                 <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>  
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>salame</span>
+                <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                    <span>{{opcional.tipo}}</span>
                 </div>
            </div>
 
@@ -51,7 +43,35 @@
 
 <script>
 export default {
-    nome: "BurgerForm"
+    nome: "BurgerForm",
+    data() {
+        return {
+            paes: null,
+            carnes: null,
+            opcionaisdata: null,
+            nome: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: null
+        }
+    },
+    methods: {
+      async getIngredientes() {
+
+          const req = await fetch("http://localhost:3000/ingredientes");
+          const data = await req.json();
+
+          this.paes = data.paes;
+          this.carnes = data.carnes;
+          this.opcionaisdata = data.opcionais;
+
+      }  
+    },
+    mounted() {
+        this.getIngredientes()
+    },
 }
 </script>
 
