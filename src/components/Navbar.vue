80f5a072d2da7c883c51a09a2a40342066b48e7a
mesaque <mesaque1212@gmail.com>
2022-04-10 00:33:54 -0300

    criando um novo router-link e alterando os dados

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+        </router-link>
+            <img src="" id="logo">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/Pedidos">Pedidos</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    nome: 'Navbar'
+}
+</script>
