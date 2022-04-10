3482b33218693d08a99300d098da888898a43e27
mesaque <mesaque1212@gmail.com>
2022-04-10 13:43:24 -0300

    Adicionando css scoped

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,50 @@
 <template>
+  <div>
     <div id="nav">
-        <router-link to="/">
-        </router-link>
-            <img src="" id="logo">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/Pedidos">Pedidos</router-link>
+      <router-link id="logo-url" to="/">
+        <img id="logo" :src="logo" :alt="alt">
+      </router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/pedidos">Pedidos</router-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    nome: 'Navbar'
+  name: "Navbar",
+  props: ["logo", "alt"]
 }
 </script>
+
+<style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    #nav #logo-url {
+        margin: auto;
+        margin-left: 0;        
+    }
+
+    #logo {
+        width: 40px;
+        height: 40px;
+    }
+
+    #nav a {
+        color: #FCBA03;
+        text-decoration: none;
+        margin: 12px;
+        transition: .5s;
+    }
+
+    #nav a:hover {
+        color: #FFF;
+    }
+</style>
