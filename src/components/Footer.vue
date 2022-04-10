f673f53b565faa1d0443d0c84f620b3dfbb341b3
mesaque <mesaque1212@gmail.com>
2022-04-10 13:43:03 -0300

    Adicionando css scoped  e o footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,24 @@
 <template>
-    <p>
-        Footer
-    </p>
+  <div id="footer">
+    <p>Make Your Burger &copy; 2021</p>
+  </div>
 </template>
 
 <script>
 export default {
-    nome: "Footer"
+  name: "Footer"
 }
 </script>
+
+<style scoped>
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+</style>
