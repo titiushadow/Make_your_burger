b313c32e8b0f391933b5c8d017a6d40ce6a2fd22
mesaque <mesaque1212@gmail.com>
2022-04-10 00:35:06 -0300

    Fazendo alteração nas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,9 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/pedidos',
+    name: 'pedidos',
+    component: () => import(/* webpackChunkName: "pedidos" */ '../views/Pedidos.vue')
   }
 ]
 
