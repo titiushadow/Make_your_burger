a117da318b66c6eb26d35a3091aea721077dd870
mesaque <mesaque1212@gmail.com>
2022-04-10 13:42:45 -0300

    Adicionando imagem e footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <Navbar />
-  <router-view/>
-  <Footer />
-
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
@@ -13,6 +16,33 @@ export default {
   components: {
     Navbar,
     Footer
-  }
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
+  },
 }
 </script>
+
+<style>
+  *{
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 250px;  
+  }
+   
+   h1 {
+     text-align: center;
+     font-size: 42px;
+     margin-bottom: 30px;
+     color: #222;
+   }
+</style>
