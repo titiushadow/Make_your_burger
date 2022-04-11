e6378879dd984850f90baf777d96648635cb014d
mesaque <mesaque1212@gmail.com>
2022-04-10 23:30:36 -0300

    Adicionando o componente dashboard em Pedidos

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="main-container">
-    <h1>Pedidos</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue'
+export default {
+   nome: "Pedidos",
+   components: { 
+    Dashboard  
+  },
+}
+</script>
